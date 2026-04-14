50029ff7546cd7c3126e0d0e2643dcbb0c891538
Muhammed Shahid vk <119295807+shahidmhd@users.noreply.github.com>
2026-04-14 12:58:29 +0530

    Add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/evocafashionstore',
+  assetPrefix: '/evocafashionstore/',
   images: {
     unoptimized: true,
     remotePatterns: [
